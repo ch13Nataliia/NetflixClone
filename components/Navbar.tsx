39aa0e65a8cb6818a307a7b233d61c0c4de9a453
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-02-11 10:50:20 +0000

    added mobile navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,23 @@
 import NavbarItem from './NavbarItem';
 import { BsChevronDown } from 'react-icons/bs';
-
+import MobileMenu from './MobileMenu';
+import { useCallback, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { FaBell } from 'react-icons/fa6';
+import AccountMenu from './AccountMenu';
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
+
+
   return (
     <nav className="w-full fixed z-40">
       <div
@@ -36,9 +52,29 @@ const Navbar = () => {
           <NavbarItem label="My List" />
           <NavbarItem label="Browse by languages" />
         </div>
-        <div className="lg:hidden flex flex-row items-center gap2 ml-8 cursor-pointer relative">
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap2 ml-8 cursor-pointer relative"
+        >
           <p className="text-white text-sm">Browse</p>
-          <BsChevronDown className="text-white transition" />
+          <BsChevronDown className={`w-4 text-white fill-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`}/>
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <FaSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <FaBell />
+          </div>
+          <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-green.png" />
+            </div>
+            <BsChevronDown  className={`w-4 text-white fill-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`}/>
+            <AccountMenu visible={showAccountMenu} />
+          </div>
         </div>
       </div>
     </nav>
