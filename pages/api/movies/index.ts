fc87f341a68c83521ccf5281e3a43858d36c396d
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-02-12 15:37:24 +0000

    added MovieList

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,24 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
 import prismadb from '@/lib/prismadb';
-import { NextApiRequest, NextApiResponse } from 'next';
+import serverAuth from "@/lib/serverAuth";
 
-import serverAuth from '@/lib/serverAuth';
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method !== 'GET') {
-    return res.status(405).end();
-  }
-
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    await serverAuth(req)
+    if (req.method !== 'GET') {
+      return res.status(405).end();
+    }
 
-    const movies = prismadb.movie.findMany()
-    return res.status(200).json(movies)
-  } catch(error) {
-    console.log(error);
-    return res.status(400).end();
+    await serverAuth(req);
+
+    const movies = await prismadb.movie.findMany();
+
+    return res.status(200).json(movies);
+  } catch (error) {
+    console.log({ error })
+    return res.status(500).end();
   }
 }
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Navbar from '@/components/Navbar';
 
 import Billboard from '@/components/Billboard';
 import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,12 +23,13 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
-        <MovieList />
+        <MovieList title="Trending Now" data={movies}/>
       </div>
     </>
   );
