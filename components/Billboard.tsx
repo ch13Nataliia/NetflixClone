303a053e37ff75470fc7b8dc1dd0bc0d4756ee47
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-02-13 14:44:56 +0000

    added zustand and updated moreinfo button

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,11 +1,16 @@
 import useBillboard from '@/hooks/useBillboard';
-import React from 'react';
-import { AiOutlineInfoCircle } from "react-icons/ai";
+import React, { useCallback } from 'react';
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 import PLayButton from './PlayButton';
-
+import useInfoModal from '@/hooks/useInfoModule';
 
 const Billboard = () => {
   const { data } = useBillboard();
+
+  const { openModal } = useInfoModal();
+  const handelOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
 
   return (
     <div className="relative h-[56.25w]">
@@ -42,9 +47,12 @@ const Billboard = () => {
           {data?.descrition}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
-          <PLayButton movieId={data?.id}/>
-          <button className="text-white bg-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-autotext-xs lg:text-lg flex flex-row items-center hover:bg-opacity-20 transition">
-          <AiOutlineInfoCircle className='mr-1 '/>
+          <PLayButton movieId={data?.id} />
+          <button
+            onClick={handelOpenModal}
+            className="text-white bg-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-autotext-xs lg:text-lg flex flex-row items-center hover:bg-opacity-20 transition"
+          >
+            <AiOutlineInfoCircle className="mr-1 " />
             More Info
           </button>
         </div>
