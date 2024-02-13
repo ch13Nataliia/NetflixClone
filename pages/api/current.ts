b028eccdbacac5f6b4274cab5cf7935ce1ce507d
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-02-13 15:32:51 +0000

    fixed res

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -1,14 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import serverAuth from '@/lib/serverAuth';
+import { NextApiRequest, NextApiResponse } from "next";
+import serverAuth from "@/lib/serverAuth";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  if (req.method != 'GET') {
-    return res.status(405).end();
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
+    if (req.method !== 'GET') {
+      return res.status(405).end();
+    }
+
     const { currentUser } = await serverAuth(req, res);
 
     return res.status(200).json(currentUser);
